ff5dc11b5744d4995c2f87ea3f86e29bd61d08cf
Nick Niemeir <nick.niemeir@gmail.com>
2012-11-09 07:54:07 +0100

    adding install script, move vim to .vim

---
+++ instal.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+var path = require('path')
+  , fs = require('fs')
+  , sh = require('shelljs')
+
+var HOME = process.env.HOME
+
+var OLD = path.join(HOME, 'old.dot')
+
+function backup (file) {
+  sh.mv(path.join(HOME, file), path.join(OLD, file))
+  sym(file)
+}
+
+function sym (file) {
+  fs.symlink(path.join(__dirname, file), path.join(HOME, file))
+}
+
+function main () {
+  sh.mkdir(OLD)
+  ;['.vimrc', '.gitconfig', '.vim', '.jshintrc']
+    .forEach(backup)
+}
+
+main()
